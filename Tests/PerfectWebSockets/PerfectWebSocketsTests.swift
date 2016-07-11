f59ae1e14ae11323e1e27b0d847d7c6dbf362fd0
Kyle Jessup <badthing@gmail.com>
2016-07-11 15:46:44 -0400

    Removed bad test

--- Tests/PerfectWebSockets/PerfectWebSocketsTests.swift
+++ Tests/PerfectWebSockets/PerfectWebSocketsTests.swift
@@ -3,11 +3,8 @@ import XCTest
 
 class PerfectWebSocketsTests: XCTestCase {
     func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        XCTAssertEqual(PerfectWebSockets().text, "Hello, World!")
+		
     }
-
 
     static var allTests : [(String, (PerfectWebSocketsTests) -> () throws -> Void)] {
         return [
