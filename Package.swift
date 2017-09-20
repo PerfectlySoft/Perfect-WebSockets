aabbb88a6471602ca8b551588fc24da8d5cb7de9
Kyle Jessup <badthing@gmail.com>
2017-09-20 14:49:07 -0400

    Bumped to 3.0

--- Package.swift
+++ Package.swift
@@ -1,30 +1,11 @@
-//
-//  Package.swift
-//  PerfectWebSockets
-//
-//  Created by Kyle Jessup on 4/20/16.
-//	Copyright (C) 2016 PerfectlySoft, Inc.
-//
-//===----------------------------------------------------------------------===//
-//
-// This source file is part of the Perfect.org open source project
-//
-// Copyright (c) 2015 - 2016 PerfectlySoft Inc. and the Perfect project authors
-// Licensed under Apache License v2.0
-//
-// See http://perfect.org/licensing.html for license information
-//
-//===----------------------------------------------------------------------===//
-//
-
+// Generated automatically by Perfect Assistant Application
+// Date: 2017-09-20 18:45:32 +0000
 import PackageDescription
-
-let urls = [
-    "https://github.com/PerfectlySoft/Perfect-HTTP.git"
-]
-
 let package = Package(
-    name: "PerfectWebSockets",
-    targets: [],
-    dependencies: urls.map { .Package(url: $0, majorVersion: 2) }
+	name: "PerfectWebSockets",
+	targets: [],
+	dependencies: [
+		.Package(url: "https://github.com/PerfectlySoft/Perfect-HTTP.git", majorVersion: 3),
+		.Package(url: "https://github.com/PerfectlySoft/Perfect-Crypto.git", majorVersion: 3),
+	]
 )
